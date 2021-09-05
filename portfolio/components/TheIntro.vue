d87cfed99a234e0e001177ca3b240f8ce8018802
Rodriguezlui002 <58277521+Rodriguezlui002@users.noreply.github.com>
2021-09-05 14:15:43 -0700

    Adjustments

--- portfolio/components/TheIntro.vue
+++ portfolio/components/TheIntro.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="about" class="h-auto text-gray-800 px-6">
+    <!-- <div id="about" class="h-auto text-gray-800 px-6">
         <div class="mx-auto w-9/10 max-w-3xl flex justify-center">
             <div class="flex-grow my-auto">
                 <h4 class="text-xl font-bold tracking-wide">Hi! I'm Luis.</h4>
@@ -8,6 +8,15 @@
             <div class="h-80 flex-grow">
                 <img class="w-full h-full" src="@/assets/E-learning.svg" alt="Character">
             </div>
+        </div>
+    </div> -->
+    <div class="flex mx-auto max-w-3xl">
+        <div>
+            <h4 class="text-xl font-bold tracking-wide">Hi! I'm Luis.</h4>
+            <p class="text-lg">A senior at <span class="text-cwu font-semibold">Central Washington University</span>. Studying computer science with a passion for web developement.</p>
+        </div>
+        <div class="w-2/5 max-w-xs">
+            <img src="@/assets/E-learning.svg" alt="Character">
         </div>
     </div>
 </template>
