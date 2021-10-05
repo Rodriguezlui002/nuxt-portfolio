08e737095ceee4eda92bc6f94f27c61dd9685ea0
Rodriguezlui002 <58277521+Rodriguezlui002@users.noreply.github.com>
2021-10-05 12:58:57 -0700

    Added favicon, removed background-position: fixed.

--- portfolio/components/TheHero.vue
+++ portfolio/components/TheHero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-5/6 h-full rounded flex flex-wrap content-center justify-center bg-hero bg-center bg-cover bg-fixed">
+    <div class="w-5/6 h-full rounded flex flex-wrap content-center justify-center bg-hero bg-center bg-cover">
         <div class="text-gray-50 text-center">
             <h1 class="text-5xl font-semibold tracking-wider mb-2">Luis Rodriguez</h1>
             <h3 class="text-xl font-light mb-10">Web Developer in training</h3>
